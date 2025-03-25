85f24437c885b38bec99cb91f89ff042edbcdfdb
Hrishav Ranjan <rishavranjan2395@gmail.com>
2025-03-25 23:45:52 +0530

    Project List Update

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,6 +1,6 @@
-import project1_img from '../assets/project_1.svg';
-import project2_img from '../assets/project_2.svg';
-import project3_img from '../assets/project_3.svg';
+import project1_img from '../assets/Project_JP.png';
+import project2_img from '../assets/Project_MP.png';
+import project3_img from '../assets/Project_QR.png';
 import project4_img from '../assets/project_4.svg';
 import project5_img from '../assets/project_5.svg';
 import project6_img from '../assets/project_6.svg';
@@ -8,39 +8,39 @@ import project6_img from '../assets/project_6.svg';
 const mywork_data = [
     {
         w_no: 1,
-        w_name: "Web Design", // Updated project name
-        w_img: project1_img,
-        link: "" // Update this with the actual link for project 1
+        w_name: "Journey Planner",
+        w_img: project1_img, 
+        link: "https://home-page-for-journey-planner.vercel.app/" 
     },
     {
         w_no: 2,
-        w_name: "E-commerce Site", // Updated project name
+        w_name: "Portfolio Website",
         w_img: project2_img,
-        link: "" // Update this with the actual link for project 2
+        link: "https://hrishav-protfolio.vercel.app/" 
     },
     {
         w_no: 3,
-        w_name: "Portfolio Website", // Updated project name
+        w_name: "Qr Code Generator",
         w_img: project3_img,
-        link: "" // Update this with the actual link for project 3
+        link: "https://hrishavranjan.github.io/QR-Code-Generator/" 
     },
     {
         w_no: 4,
-        w_name: "Blogging Platform", // Updated project name
+        w_name: "Tic-Tac-Toe-Game",
         w_img: project4_img,
-        link: "" // Update this with the actual link for project 4
+        link: "https://hrishavranjan.github.io/Tic-Tac-Toe-Game/" 
     },
     {
         w_no: 5,
-        w_name: "Social Media App", // Updated project name
+        w_name: "Social Media App",
         w_img: project5_img,
-        link: "" // Update this with the actual link for project 5
+        link: "" 
     },
     {
         w_no: 6,
-        w_name: "Task Management Tool", // Updated project name
+        w_name: "Task Management Tool",
         w_img: project6_img,
-        link: "" // Update this with the actual link for project 6
+        link: "" 
     },
 ];
 
